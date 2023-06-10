f4918986e854a2816c43016b938b58ba1441ed9a
Maloth Shivalal Prasad <132836718+shivalalprasad@users.noreply.github.com>
2023-06-10 02:41:05 -0700

    meta confusion

--- app/layout.js
+++ app/layout.js
@@ -9,14 +9,9 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Department of Physics GDC KMR",
-  description: "Discover the Physics Department website of GDC KMR. Explore lecture notes, activities, old question papers, practical guides, college blogs, and more. Enhance your understanding, prepare for exams, and stay updated on the latest in physics. Join us on this educational journey today!";
-  name:"google-site-verification",
-  content: "emhlqdbv3W9xP9-4GOGvH9iAwu6BzeYsy6tenMFsgqE";
+  description: "Discover the Physics Department website of GDC KMR. Explore lecture notes, activities, old question papers, practical guides, college blogs, and more. Enhance your understanding, prepare for exams, and stay updated on the latest in physics. Join us on this educational journey today!",
 };
-// export const metadata = {
-  
-// }
-{/* <meta name="google-site-verification" content="emhlqdbv3W9xP9-4GOGvH9iAwu6BzeYsy6tenMFsgqE" /> */}
+ <meta name="google-site-verification" content="emhlqdbv3W9xP9-4GOGvH9iAwu6BzeYsy6tenMFsgqE" />
 
 export default function RootLayout({ children }) {
   return (
