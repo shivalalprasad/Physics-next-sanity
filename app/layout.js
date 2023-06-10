a1ce27700e07b126cfc8ecee54ab50d4c5cb208b
Maloth Shivalal Prasad <132836718+shivalalprasad@users.noreply.github.com>
2023-06-10 02:24:04 -0700

    google site verification

--- app/layout.js
+++ app/layout.js
@@ -9,13 +9,13 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Department of Physics GDC KMR",
-  description: "Discover the Physics Department website of GDC KMR. Explore lecture notes, activities, old question papers, practical guides, college blogs, and more. Enhance your understanding, prepare for exams, and stay updated on the latest in physics. Join us on this educational journey today!",
-
-};
-export const metadata = {
+  description: "Discover the Physics Department website of GDC KMR. Explore lecture notes, activities, old question papers, practical guides, college blogs, and more. Enhance your understanding, prepare for exams, and stay updated on the latest in physics. Join us on this educational journey today!";
   name:"google-site-verification",
-  content: "emhlqdbv3W9xP9-4GOGvH9iAwu6BzeYsy6tenMFsgqE"
-}
+  content: "emhlqdbv3W9xP9-4GOGvH9iAwu6BzeYsy6tenMFsgqE";
+};
+// export const metadata = {
+  
+// }
 {/* <meta name="google-site-verification" content="emhlqdbv3W9xP9-4GOGvH9iAwu6BzeYsy6tenMFsgqE" /> */}
 
 export default function RootLayout({ children }) {
