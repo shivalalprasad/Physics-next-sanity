fba7c0d728130aa2c75af3c7944c73c049e90eea
Maloth Shivalal Prasad <132836718+shivalalprasad@users.noreply.github.com>
2023-06-11 00:21:22 -0700

    google meta

--- app/layout.js
+++ app/layout.js
@@ -3,18 +3,18 @@ import { Inter } from "next/font/google";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Head from 'next/Head'
 import { createClient } from "next-sanity";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
-  title: "Department of Physics GDC KMR",
-  description: "Discover the Physics Department website of GDC KMR. Explore lecture notes, activities, old question papers, practical guides, college blogs, and more. Enhance your understanding, prepare for exams, and stay updated on the latest in physics. Join us on this educational journey today!",
-};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+      <meta name="google-site-verification" content="emhlqdbv3W9xP9-4GOGvH9iAwu6BzeYsy6tenMFsgqE" />
+      </Head>
       <body className={inter.className}>
         <Header />
         <Navbar/>
--- app/page.js
+++ app/page.js
@@ -1,9 +1,9 @@
 // import Image from "next/image";
-export const metadata = {
-  name: "google-site-verification",
-  content:"emhlqdbv3W9xP9-4GOGvH9iAwu6BzeYsy6tenMFsgqE",
-};
 
+export const metadata = {
+  title: "Department of Physics GDC KMR",
+  description: "Discover the Physics Department website of GDC KMR. Explore lecture notes, activities, old question papers, practical guides, college blogs, and more. Enhance your understanding, prepare for exams, and stay updated on the latest in physics. Join us on this educational journey today!",
+};
 
 export default function Home() {
   // fill=true // {true} | {false}
