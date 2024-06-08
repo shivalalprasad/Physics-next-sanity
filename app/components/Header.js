0df1d47d5a08552e18fa02d865a44a5a00f7bf6e
Maloth Shivalal Prasad <132836718+shivalalprasad@users.noreply.github.com>
2024-06-08 22:34:04 +0530

    appling the logo that was not loading

--- app/components/Header.js
+++ app/components/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
     <div className="container mx-auto flex flex-wrap flex-col md:flex-row items-center">
       <a href="https://physics-demo-gdckmr.web.app/"><span
           className="flex title-font font-medium items-center text-gray-900 md:mb-0">
-          <img src="https://physics.gdckmr.repl.co/utills/images/gdclogo.png"
+          <img src="https://4425508f-27d9-458e-aba2-9c25f90bb643-00-2bree4xj3m2oc.riker.replit.dev/utills/images/gdclogo.png"
            alt="Logo" width={30} height={30}
             className="d-inline-block align-text-top"/>
           <span className="ml-3 text-xl">GDC KMR</span>
